85e3a5afa7ab489cab0b2131394455febf5c5456
alireza1111 <alireza.davoudian@snd.gu.se>
2020-07-28 11:40:03 +0200

    Function definition and users.password added to Migraion + docker-entrypoint script activated + true image name for db used.

--- LoginEntityFramnework/Migrations/20200728085617_AddSpLoginFunction.Designer.cs
+++ LoginEntityFramnework/Migrations/20200728085617_AddSpLoginFunction.Designer.cs
@@ -2,15 +2,17 @@
 using LoginEntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace LoginEntityFramework.Migrations
 {
     [DbContext(typeof(LoginContext))]
-    partial class LoginContextModelSnapshot : ModelSnapshot
+    [Migration("20200728085617_AddSpLoginFunction")]
+    partial class AddSpLoginFunction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LoginEntityFramnework/Migrations/20200728085617_AddSpLoginFunction.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoginEntityFramework.Migrations
+{
+    public partial class AddSpLoginFunction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "password",
+                table: "users",
+                type: "VARCHAR(100)",
+                nullable: true);
+
+            migrationBuilder.Sql(@"CREATE OR REPLACE FUNCTION sp_login(varchar(100), varchar(100)) RETURNS integer
+as $$
+begin
+   return (select count(1) FROM users WHERE name = $1 and password = $2);
+commit;
+end; 
+$$ LANGUAGE plpgsql;");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "password",
+                table: "users");
+        }
+    }
+}
